7df5657ff7bfb6e79e560a16663040a06d3c6db9
Wilson Ponseca <128311166+Sentinail@users.noreply.github.com>
2025-10-25 20:57:28 +0800

    #1

--- src/main/java/com/mycompany/ballcollisionsimulation/BallCollisionSimulation.java
+++ src/main/java/com/mycompany/ballcollisionsimulation/BallCollisionSimulation.java
@@ -11,6 +11,6 @@ package com.mycompany.ballcollisionsimulation;
 public class BallCollisionSimulation {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World #1!");
     }
 }
